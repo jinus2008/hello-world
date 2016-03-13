fd1b84195a698636df4ca08aaff92a6a363123ef
jinus2008 <jinus2008@gmail.com>
2016-03-13 16:49:17 +0900

    add cpc.boy.jp
    add cpc.boy.jp with index.pp

---
+++ cpc.boy.jp/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>未名小站</title>
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+  <style type="text/css">
+.comment {
+	font-size: 16px;
+	text-align: center;
+}
+.center {
+	text-align: center;
+}
+
+</style>
+
+</head>
+
+<body>
+  <div data-role="page">
+    <div data-role="header">
+      <h1>欢迎光临</h1>
+    </div><!-- /header -->
+    <div role="main" class="ui-content">
+    <div class="comment">本站提供工具类网站制作，为您的业务操作提供便利支持，提高工作效率。</div>
+    <p>&nbsp;</p>
+<div class="center">
+网站一览：
+<table width="238" border="1" cellspacing="1" style="border-collapse:collapse">
+  <tr>
+    <td width="130"><a href="http://seidaijp.com/index.html" target="_blank">手机高价回收</a></td>
+    <td width="95">运营中</td>
+  </tr>
+  <tr>
+    <td><a href="./bofu" target="_blank">博富预约系统</a></td>
+    <td>建设中</td>
+  </tr>
+  <tr>
+    <td><a href="oldSite/xingwangtowel.PNG" target="_blank">毛巾贩卖</a></td>
+    <td>已中止使用</td>
+  </tr>
+</table></div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div align="center">
+联系方式：
+<table width="238" border="1" cellspacing="1" style="border-collapse:collapse">
+  <tr>
+    <td width="50">电话</a></td>
+    <td>080-4889-8686</td>
+  </tr>
+  <tr>
+    <td width="50">Email</a></td>
+    <td><a href="mailto:jinus2008@gmail.com">jinus2008@gmail.com</a></td>
+  </tr>
+  <tr>
+    <td>微信</td>
+    <td>jinus2008</td>
+  </tr>
+  <tr>
+    <td>QQ</td>
+    <td>860192749</td>
+  </tr>
+  <tr>
+    <td>留言</td>
+    <td>点这里留言</td>
+  </tr>
+</table></div>
+
+<br>
+<br>
+    </div>
+    <div data-role="footer">
+        <h4>&copy; 2016 未名小站</h4>
+        <p class="center">您是本页面的第<span id="counter"></span>位访客</p>
+    </div><!-- /footer -->
+  </div>
+</body>
+<script type=text/javascript src=counter/counter_ip.php?id=index></script>
+
+</html>
